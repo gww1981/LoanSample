a71052b20f1ea50ffd5f02f45c03ed2b64952c56
郭文武 <guowenwu1981815@163.com>
2020-07-27 23:19:55 +0800

    增加领域实体属性

--- LoanSample.Domain/Customer.cs
+++ LoanSample.Domain/Customer.cs
@@ -7,13 +7,31 @@ namespace LoanSample.Domain
 {
     public class Customer : AggregateRoot<Guid>
     {
+        private readonly List<LinkMan> _linkMen;
+
         public string Name { get; set; }
 
         public string Address { get; set;  }
 
-        public ICollection<LinkMan> linkMen { get; set; }
+        public IReadOnlyCollection<LinkMan> LinkMen 
+        {
+            get { return _linkMen.AsReadOnly(); }
+        }
 
+        
+        public Customer()
+        {
+            _linkMen = new List<LinkMan>();
+        }
 
+        public void AddLinkMan(LinkMan linkMan)
+        {
+            _linkMen.Add(linkMan);
+        }
 
+        public void RemoveLinkMan(LinkMan linkMan)
+        {
+            _linkMen.Remove(linkMan);
+        }
     }
 }
--- LoanSample.Domain/LinkMan.cs
+++ LoanSample.Domain/LinkMan.cs
@@ -10,7 +10,5 @@ namespace LoanSample.Domain
         public string Name { get; set; }
 
         public string Mobile { get; set; }
-
-
     }
 }
