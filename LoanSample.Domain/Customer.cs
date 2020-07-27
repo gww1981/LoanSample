5110c16253543663419ef917c9cf2e5035a5c715
Colin <475464309@qq.com>
2020-07-27 13:08:19 +0800

    添加项目文件。

---
+++ LoanSample.Domain/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Domain.Entities;
+
+namespace LoanSample.Domain
+{
+    public class Customer : AggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set;  }
+
+        public ICollection<LinkMan> linkMen { get; set; }
+
+
+
+    }
+}
---
+++ LoanSample.Domain/LinkMan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Domain.Entities;
+
+namespace LoanSample.Domain
+{
+    public class LinkMan : Entity<Guid>
+    {
+        public string Name { get; set; }
+
+        public string Mobile { get; set; }
+
+
+    }
+}
